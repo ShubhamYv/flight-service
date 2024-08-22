42e43e5eaee4fc18bf5642bb99c02114fd9deec4
ShubhamYv <shubhamyadav32100@gmail.com>
2024-08-22 21:01:14 +0530

    refactor the crud-repository code

--- src/repositories/crud-repository.js
+++ src/repositories/crud-repository.js
@@ -6,57 +6,32 @@ class CrudRepository {
   }
 
   async create(data) {
-    try {
-      const response = await this.model.create(data);
-      return response;
-    } catch (error) {
-      Logger.error("Something went wrong in the CrudRepository::create");
-      throw error;
-    }
+    const response = await this.model.create(data);
+    return response;
   }
 
   async destroy(data) {
-    try {
-      const response = await this.model.destroy({
-        where: { id: data }
-      });
-      return response;
-    } catch (error) {
-      Logger.error("Something went wrong in the CrudRepository::destroy");
-      throw error;
-    }
+    const response = await this.model.destroy({
+      where: { id: data }
+    });
+    return response;
   }
 
   async get(data) {
-    try {
-      const response = await this.model.findByPk(data);
-      return response;
-    } catch (error) {
-      Logger.error("Something went wrong in the CrudRepository::get");
-      throw error;
-    }
+    const response = await this.model.findByPk(data);
+    return response;
   }
 
   async getAll() {
-    try {
-      const response = await this.model.findAll();
-      return response;
-    } catch (error) {
-      Logger.error("Something went wrong in the CrudRepository::getAll");
-      throw error;
-    }
+    const response = await this.model.findAll();
+    return response;
   }
 
   async update(id, data) {
-    try {
-      const response = await this.model.update(data, {
-        where: { id: id }
-      });
-      return response;
-    } catch (error) {
-      Logger.error("Something went wrong in the CrudRepository::update");
-      throw error;
-    }
+    const response = await this.model.update(data, {
+      where: { id: id }
+    });
+    return response;
   }
 }
 
